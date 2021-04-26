5fba855a35e737d849896dd8094b92bd0b4493ef
Connor Holland <supersoccergoalie123@gmail.com>
2021-04-26 17:48:31 -0600

    commit tree 3294471dafd4ccf742458acdcfbd7cfdb6789863

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+
+let drivers = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  let array = [...drivers]
+  array.push(name)
+  return array
+}
+
+function prependDriver(name) {
+  let array = [...drivers]
+  array.unshift(name)
+  return array
+}
+
+function removeLastDriver() {
+  let array = [...drivers]
+  array.pop()
+  return array
+}
+
+function  removeFirstDriver() {
+  let array = [...drivers]
+  array.shift()
+  return array
+}
